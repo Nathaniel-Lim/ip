c2c3ae96a3200929ec1241cce5b66a29e835ce59
Nathaniel-Lim <Cloud9RMJ@gmail.com>
2026-08-20 10:42:51 +0800

    Modified chatbot name and added greeting

---
+++ src/main/java/Melodie.java
@@ -0,0 +1,20 @@
+public class Melodie {
+    public static String name = "Melodie";
+
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        String banner = " __  __      _           _ _\n" +
+                "|  \\/  | ___| | ___   __| (_) ___\n" +
+                "| |\\/| |/ _ \\ |/ _ \\ / _` | |/ _ \\\n" +
+                "| |  | |  __/ | (_) | (_| | |  __/\n" +
+                "|_|  |_|\\___|_|\\___/ \\__,_|_|\\___|\n" +
+                "\n" +
+                "          ♪  ♫  ♪";
+        System.out.println(banner);
+        System.out.println("Hello ♪ I'm " + name + "~");
+        System.out.println("What master piece shall we play?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Farewell, come play with me again :D");
+        System.out.println("____________________________________________________________");
+    }
+}
