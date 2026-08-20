14b1f796ddc59c1da12c17f9b358bcbf0998f370
Nathaniel-Lim <Cloud9RMJ@gmail.com>
2026-08-20 10:59:40 +0800

    Updated code structure for Level-0

--- src/main/java/Melodie.java
+++ src/main/java/Melodie.java
@@ -1,20 +1,34 @@
 public class Melodie {
     public static String name = "Melodie";
+    public static String banner =
+            " __  __      _           _ _\n" +
+            "|  \\/  | ___| | ___   __| (_) ___\n" +
+            "| |\\/| |/ _ \\ |/ _ \\ / _` | |/ _ \\\n" +
+            "| |  | |  __/ | (_) | (_| | |  __/\n" +
+            "|_|  |_|\\___|_|\\___/ \\__,_|_|\\___|\n" +
+            "\n" +
+            "          ♪  ♫  ♪";
 
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        String banner = " __  __      _           _ _\n" +
-                "|  \\/  | ___| | ___   __| (_) ___\n" +
-                "| |\\/| |/ _ \\ |/ _ \\ / _` | |/ _ \\\n" +
-                "| |  | |  __/ | (_) | (_| | |  __/\n" +
-                "|_|  |_|\\___|_|\\___/ \\__,_|_|\\___|\n" +
-                "\n" +
-                "          ♪  ♫  ♪";
-        System.out.println(banner);
-        System.out.println("Hello ♪ I'm " + name + "~");
-        System.out.println("What master piece shall we play?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Farewell, come play with me again :D");
-        System.out.println("____________________________________________________________");
+        System.out.println(printIntro());
+        System.out.println(printFarewell());
+
+    }
+
+    public static String printIntro() {
+        String returnString =
+                "____________________________________________________________\n" +
+                banner + "\n" +
+                "Hello ♪ I'm " + name + "~\n" +
+                "What master piece shall we play?\n" +
+                "____________________________________________________________\n";
+        return returnString;
+    }
+
+    public static String printFarewell() {
+        String returnString =
+                "Farewell, come play with me again :D\n" +
+                "____________________________________________________________\n";
+        return returnString;
     }
 }
